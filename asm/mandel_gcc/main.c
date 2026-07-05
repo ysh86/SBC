be7d9f9f56c4c1185bc4f12451b99c6d2a823d93
Yoshiharu <ysh86@users.noreply.github.com>
2026-07-05 10:25:31 +0900

    68K8: add mandel by gcc

--- asm/mandel_gcc/main.c
+++ asm/mandel_gcc/main.c
@@ -5,7 +5,7 @@
 #define UART_DATA      (*(volatile uint8_t *)(UART_BASE + 1))
 #define UART_TX_READY  0x02
 
-static void uart_putc(char c)
+static inline void uart_putc(char c)
 {
     while ((UART_STATUS & UART_TX_READY) == 0)
         ;
@@ -19,10 +19,40 @@ static void uart_puts(const char *s)
         uart_putc(*s++);
 }
 
-static const char message[] = "Hello, user mode world!\r\n";
+
+static inline void myputchar(int c)
+{
+    uart_putc(c);
+}
 
 int main(void)
 {
-    uart_puts(message);
-    return sizeof(message);
+    int x, y, i;
+    float ca, cb, a, b, t;
+
+    for (y = -12; y <= 12; ++y) {
+        for (x = -39; x <= 39; ) {
+            ca = x * 0.0458f;
+            cb = y * 0.08333f;
+            a = ca;
+            b = cb;
+            for (i = 0; i <= 15; ++i) {
+                t = a*a - b*b + ca;
+                b = 2 * a * b + cb;
+                a = t;
+                if ((a * a + b * b) > 4) {
+                    if (i > 9)
+                        i = i + 7;
+                    myputchar(48+i);
+                    goto x_next;
+                }
+            }
+            myputchar(' ');
+x_next:
+            ++x;
+        }
+        myputchar('\n');
+    }
+
+    return y;
 }
